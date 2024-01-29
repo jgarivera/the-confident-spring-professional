8dd9774413dc9ad7f14107c210898c595e8cde2e
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-01-29 21:26:33 +0800

    Specify request param value in invoices controller

--- 05-my-fancy-pdf-invoices-spring-mvc/src/main/java/com/jgarivera/web/InvoicesController.java
+++ 05-my-fancy-pdf-invoices-spring-mvc/src/main/java/com/jgarivera/web/InvoicesController.java
@@ -5,9 +5,11 @@ import com.jgarivera.service.InvoiceService;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -28,7 +30,9 @@ public class InvoicesController {
     }
 
     @PostMapping("/invoices")
-    public Invoice createInvoice(@RequestParam("user_id") @NotBlank String userId, @RequestParam @Min(10) @Max(50) Integer amount) {
+    public Invoice createInvoice(
+            @RequestParam("user_id") @NotBlank String userId,
+            @RequestParam("amount") @Min(10) @Max(50) Integer amount) {
         return invoiceService.create(userId, amount);
     }
 }
